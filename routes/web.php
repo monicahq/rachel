daa5437ae0217d773cd2317cc545a00be4df1b8f
Regis Freyd <djaiss@users.noreply.github.com>
2025-09-28 10:34:54 -0400

    test: add rector

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory => view('welcome'));
