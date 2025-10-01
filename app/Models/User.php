a556f0a34a25e7cd8efd557e0f876e3fa0e9b69c
Alexis Saettler <alexis@saettler.org>
2025-10-01 14:36:31 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 final class User extends Authenticatable
 {
