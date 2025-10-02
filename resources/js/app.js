b91c20ae29de7097d4e86bb8253ce3bc6d526f67
Alexis Saettler <alexis@saettler.org>
2025-10-02 22:56:04 +0200

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,27 @@
-// import "./bootstrap";
+import "./bootstrap";
 
-// import "instant.page";
+import "instant.page";
 
-// // --- Turbo Drive ---
-// import * as Turbo from "@hotwired/turbo";
-// window.Turbo = Turbo;
-// Turbo.session.drive = true; // explicit (enabled by default)
+// --- Turbo Drive ---
+import * as Turbo from "@hotwired/turbo";
+window.Turbo = Turbo;
+Turbo.session.drive = true; // explicit (enabled by default)
 
-// // --- Alpine ---
-// import Alpine from "alpinejs";
-// import ajax from "@imacrayon/alpine-ajax";
+// --- Alpine ---
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import ajax from "@imacrayon/alpine-ajax";
 
-// window.Alpine = Alpine;
-// Alpine.plugin(ajax);
+Alpine.plugin(ajax);
 
-// // Start Alpine on the initial load (once)
-// document.addEventListener("DOMContentLoaded", () => {
-//   if (!document.documentElement.__alpined) {
-//     Alpine.start();
-//     document.documentElement.__alpined = true;
-//   }
-// });
+// Start Alpine on the initial load (once)
+document.addEventListener("DOMContentLoaded", () => {
+  if (!document.documentElement.__alpined) {
+    Livewire.start();
+    document.documentElement.__alpined = true;
+  }
+});
 
-// // Re-initialize Alpine after every Turbo-driven navigation
-// addEventListener("turbo:load", () => {
-//   if (window.Alpine?.initTree) Alpine.initTree(document.body);
-// });
+// Re-initialize Alpine after every Turbo-driven navigation
+addEventListener("turbo:load", () => {
+  if (window.Alpine?.initTree) Alpine.initTree(document.body);
+});
