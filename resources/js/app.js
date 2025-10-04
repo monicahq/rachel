9004209ef0d6a104ad238bc147dbe06f4ea23eaa
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-04 13:51:28 -0400

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,3 @@ import "./bootstrap";
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 Livewire.start();
-
-// Re-initialize Alpine after every Turbo-driven navigation
-// addEventListener("turbo:load", () => {
-//   if (window.Alpine?.initTree) Alpine.initTree(document.body);
-// });
