0ece5a4405d8fb0e4af142cce38f8d59b0595ba0
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-04 13:49:16 -0400

    chore: remove alpine ajax and turbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,6 @@
 import "./bootstrap";
 
-import "instant.page";
-
-// --- Turbo Drive ---
-// import * as Turbo from "@hotwired/turbo";
-// window.Turbo = Turbo;
-// Turbo.session.drive = true; // explicit (enabled by default)
-
-// --- Alpine ---
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import ajax from "@imacrayon/alpine-ajax";
-
-Alpine.plugin(ajax);
 Livewire.start();
 
 // Re-initialize Alpine after every Turbo-driven navigation
