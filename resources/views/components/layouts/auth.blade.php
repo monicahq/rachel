7916bfad25f6ba2a468dcd727aaf22d05d2312fa
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-04 13:38:55 -0400

    chore: add favicon for monica (#17)
    We had Laravel's default icon. Let's put Monica's instead.

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,3 +1,3 @@
-<x-layouts.auth.simple :title="$title ?? null">
+<x-layouts.auth.split :title="$title ?? null">
     {{ $slot }}
-</x-layouts.auth.simple>
+</x-layouts.auth.split>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,11 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon-96x96.png') }}" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}" />
+ <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
