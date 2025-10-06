d1a30e7d2ff73609ee05445996f8c7168e4ccac6
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-06 07:28:52 -0400

    Update AccountFactory.php

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -16,8 +16,6 @@ final class AccountFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array
      */
     public function definition(): array
     {
