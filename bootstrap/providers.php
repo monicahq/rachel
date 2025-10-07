0a32c81a14bf754b1f3541b771b8b7fafeaa0667
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-06 20:12:09 -0400

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use EragLaravelDisposableEmail\LaravelDisposableEmailServiceProvider;
-
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    LaravelDisposableEmailServiceProvider::class,
 ];
