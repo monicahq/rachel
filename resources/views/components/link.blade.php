c6fd70452433ef315ed6c292ac5c86f799d62e33
Regis Freyd <djaiss@users.noreply.github.com>
2025-10-07 19:57:04 -0400

    wip

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,16 +1,17 @@
 {{-- It's important that this file does NOT have a newline at the end. --}}
 
 @props([
-    'external' => false,
-    'navigate' => true,
+  'external' => false,
+  'navigate' => true,
 ])
 
-<a @if ($external) target="_blank" @endif @if ($navigate) wire:navigate @endif
-    {{ $attributes->class([
-        'inline underline',
-        'underline-offset-4',
-        'hover:decoration-[1.15px]',
-        'decoration-gray-300',
-        'hover:text-blue-600 hover:decoration-blue-400',
-        'transition-colors duration-200',
-    ]) }}>{{ $slot }}</a>
+<a @if ($external) target="_blank" @endif @if ($navigate) wire:navigate @endif {{
+  $attributes->class([
+    'inline underline',
+    'underline-offset-4',
+    'hover:decoration-[1.15px]',
+    'decoration-gray-300',
+    'hover:text-blue-600 hover:decoration-blue-400',
+    'transition-colors duration-200',
+  ])
+}}>{{ $slot }}</a>
--- resources/views/livewire/settings/password.blade.php
+++ resources/views/livewire/settings/password.blade.php
@@ -8,7 +8,9 @@ use Livewire\Volt\Component;
 
 new class extends Component {
   public string $current_password = '';
+
   public string $password = '';
+
   public string $password_confirmation = '';
 
   /**
@@ -42,9 +44,9 @@ new class extends Component {
 
   <x-settings.layout :heading="__('Update password')" :subheading="__('Ensure your account is using a long, random password to stay secure')">
     <form method="POST" wire:submit="updatePassword" class="mt-6 space-y-6">
-      <flux:input wire:model="current_password" :label="__('Current password')" type="password" required autocomplete="current-password" />
-      <flux:input wire:model="password" :label="__('New password')" type="password" required autocomplete="new-password" />
-      <flux:input wire:model="password_confirmation" :label="__('Confirm Password')" type="password" required autocomplete="new-password" />
+      <x-input wire:model="current_password" id="current_password" :label="__('Current password')" type="password" required autocomplete="current-password" />
+      <x-input wire:model="password" id="password" :label="__('New password')" type="password" required autocomplete="new-password" />
+      <x-input wire:model="password_confirmation" id="password_confirmation" :label="__('Confirm Password')" type="password" required autocomplete="new-password" />
 
       <div class="flex items-center gap-4">
         <div class="flex items-center justify-end">
