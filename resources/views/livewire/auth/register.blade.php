3934a7d882571d149136ab5cea1bf121568ad675
Alexis Saettler <alexis@saettler.org>
2025-10-08 23:21:57 +0200

    fix

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -11,39 +11,39 @@ use Livewire\Volt\Component;
 
 new #[Layout('components.layouts.guest')] class extends Component
 {
-    public string $name = '';
+  public string $name = '';
 
-    public string $email = '';
+  public string $email = '';
 
-    public string $password = '';
+  public string $password = '';
 
-    public string $password_confirmation = '';
+  public string $password_confirmation = '';
 
-    /**
-     * Handle an incoming registration request.
-     */
-    public function register(): void
-    {
-        $validated = $this->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
-        ]);
+  /**
+   * Handle an incoming registration request.
+   */
+  public function register(): void
+  {
+    $validated = $this->validate([
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+      'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+    ]);
 
-        $user = new CreateAccount(
-            email: $validated['email'],
-            password: $validated['password'],
-            name: $validated['name'],
-        )->execute();
+    $user = new CreateAccount(
+      email: $validated['email'],
+      password: $validated['password'],
+      name: $validated['name'],
+    )->execute();
 
-        event(new Registered($user));
+    event(new Registered($user));
 
-        Auth::login($user);
+    Auth::login($user);
 
-        Session::regenerate();
+    Session::regenerate();
 
-        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
-    }
+    $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
+  }
 }; ?>
 
 <div class="grid min-h-screen w-screen grid-cols-1 lg:grid-cols-2">
