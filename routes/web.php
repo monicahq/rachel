a39c93c2a0843ed7c8c21809d4dd63030b4efe4a
Alexis Saettler <alexis@saettler.org>
2025-10-13 23:25:51 +0200

    simplify

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Settings;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::get('/', fn (): View => view('welcome'))->name('home');
@@ -31,14 +30,7 @@ Route::middleware(['auth', 'verified'])->group(function (): void {
     Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
 
     Volt::route('settings/two-factor', 'settings.two-factor')
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
-                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
+        ->middleware(['password.confirm'])
         ->name('two-factor.show');
 });
 
