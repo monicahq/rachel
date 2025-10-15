9c433eaedeb9c898110b3a091e6cccab17db86e9
Alexis Saettler <alexis@saettler.org>
2025-10-15 23:39:47 +0200

    more tests

--- tests/Feature/VaultTest.php
+++ tests/Feature/VaultTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Models\User;
 use App\Models\Vault;
+use Livewire\Volt\Volt;
 
 test('users can list vaults', function (): void {
     $this->actingAs($user = User::factory()->create());
@@ -25,4 +26,15 @@ test('users can show a vault', function (): void {
     $response = $this->get(route('vaults.show', [$vault]));
     $response->assertStatus(200);
     $response->assertSee($vault->name);
+});
+
+test('user can create a vault', function (): void {
+    $this->actingAs($user = User::factory()->create());
+    $response = Volt::test('vaults.index')
+        ->set('name', 'Test Vault')
+        ->call('create');
+
+    $response
+        ->assertHasNoErrors()
+        ->assertRedirect(route('vaults.show', ['vault' => 'test-vault']));
 });
