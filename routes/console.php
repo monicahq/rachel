e8c9fba7638752dd5c67bcf46b511bd0ed644dee
Alexis Saettler <alexis@saettler.org>
2025-10-28 08:20:42 +0100

    update

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Models\AuthCode;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('sanctum:prune-expired --hours=24')->daily();
+Schedule::command('sanctum:prune-expired --hours=24')->dailyAt('01:00');
 Schedule::call(function (): void {
     AuthCode::where('expires_at', '<', now())->delete();
-})->daily();
+})->dailyAt('02:00');
