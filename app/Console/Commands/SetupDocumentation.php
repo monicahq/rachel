18a7ace822144db6e5405da4e8ee0b536c00834d
Alexis Saettler <alexis@saettler.org>
2025-11-08 20:15:08 +0100

    feat: add scribe (#42)
    Add an api documentation generator, using `knuckleswtf/scribe` package.
    
    This adds a new console artisan `scribe:setup`, which is also added to
    the `setup` composer command to automatically generate the api doc at
    installation.
    
    The documentation is accessible through `/docs` url of the application

---
+++ app/Console/Commands/SetupDocumentation.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+#[AsCommand(name: 'scribe:setup')]
+final class SetupDocumentation extends Command
+{
+    use ConfirmableTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scribe:setup
+                            {--clean : Remove database file after generating the documentation.}
+                            {--force : Force the operation to run when in production.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the api documentation.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if ($this->confirmToProceed()) {
+            $this->documentation();
+        }
+    }
+
+    /**
+     * Regenerate api documentation.
+     */
+    private function documentation(): void
+    {
+        putenv('DB_CONNECTION=docs');
+        putenv('APP_ENV=testing');
+        putenv('CACHE_DRIVER=array');
+        putenv('QUEUE_CONNECTION=sync');
+        putenv('SESSION_DRIVER=array');
+        putenv('MAIL_MAILER=log');
+        putenv('SCOUT_DRIVER=null');
+
+        if (! File::exists($database = config('database.connections.docs.database'))) {
+            File::put($database, '');
+        }
+
+        $verbosity = $this->getVerbosity();
+        $artisan = base_path('artisan');
+
+        passthru("$artisan migrate:fresh --force -q");
+        passthru("$artisan scribe:generate --force$verbosity");
+
+        if ($this->option('clean') && File::exists($database)) {
+            File::delete($database);
+        }
+    }
+
+    private function getVerbosity(): string
+    {
+        $verbosity = $this->getOutput()->getOutput()->getVerbosity();
+        if ($verbosity >= OutputInterface::VERBOSITY_DEBUG) {
+            return ' -vvv';
+        }
+
+        if ($verbosity >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
+            return ' -vv';
+        }
+
+        if ($verbosity >= OutputInterface::VERBOSITY_VERBOSE) {
+            return ' -v';
+        }
+
+        if ($verbosity <= OutputInterface::VERBOSITY_QUIET) {
+            return ' -q';
+        }
+
+        return '';
+    }
+}
--- app/Http/Controllers/Api/VaultController.php
+++ app/Http/Controllers/Api/VaultController.php
@@ -9,9 +9,18 @@ use App\Models\Vault;
 use App\Services\CreateVault;
 use App\Services\DestroyVault;
 use App\Services\UpdateVault;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Knuckles\Scribe\Attributes\BodyParam;
+use Knuckles\Scribe\Attributes\Response;
+use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 
+/**
+ * @group Vault management
+ *
+ * @subgroup Vaults
+ */
 final class VaultController extends Controller
 {
     public function __construct()
@@ -24,6 +33,7 @@ final class VaultController extends Controller
     /**
      * List all vaults.
      */
+    #[ResponseFromApiResource(JsonResource::class, Vault::class, collection: true)]
     public function index(Request $request): JsonResource
     {
         $vaults = $request->user()->account->vaults;
@@ -34,6 +44,7 @@ final class VaultController extends Controller
     /**
      * Retrieve a vault.
      */
+    #[ResponseFromApiResource(JsonResource::class, Vault::class)]
     public function show(Request $request, Vault $vault): JsonResource
     {
         return new JsonResource($vault);
@@ -42,6 +53,9 @@ final class VaultController extends Controller
     /**
      * Create a vault.
      */
+    #[ResponseFromApiResource(JsonResource::class, Vault::class, status: 201)]
+    #[BodyParam('name', description: 'The name of the vault. Max 255 characters.')]
+    #[BodyParam('description', description: 'The description of the vault. Max 65535 characters.', required: false)]
     public function store(Request $request): JsonResource
     {
         $validated = $this->validate($request, [
@@ -61,6 +75,9 @@ final class VaultController extends Controller
     /**
      * Update a vault.
      */
+    #[ResponseFromApiResource(JsonResource::class, Vault::class)]
+    #[BodyParam('name', description: 'The name of the vault. Max 255 characters.')]
+    #[BodyParam('description', description: 'The description of the vault. Max 65535 characters.', required: false)]
     public function update(Request $request, Vault $vault): JsonResource
     {
         $validated = $this->validate($request, [
@@ -81,12 +98,13 @@ final class VaultController extends Controller
     /**
      * Destroy a vault.
      */
-    public function destroy(Request $request, Vault $vault): JsonResource
+    #[Response(status: 204)]
+    public function destroy(Request $request, Vault $vault): JsonResponse
     {
         (new DestroyVault(
             vault: $vault
         ))->execute();
 
-        return new JsonResource([]);
+        return new JsonResponse(status: 204);
     }
 }
