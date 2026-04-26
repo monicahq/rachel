77766d926e605ca23b45f2c3bd8508cdc114d795
Alexis Saettler <alexis@saettler.org>
2026-04-26 23:01:09 +0200

    chore: update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'api/v1',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->throttleApi();
